8eb0f83f9e83103910d1dd6c77096f64e7012ae5
Namuun_L <mit.irnitu@gmail.com>
2023-02-14 15:09:27 +0800

    backend post

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,6 +1,8 @@
 // Requiring module like importing cors and express
+const {v4: uuid} = require("uuid")
 const express = require("express");
 const cors = require("cors");
+const fs = require("fs")
 // Port Number
 const port = 8000;
 // creating express app
@@ -11,9 +13,23 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("Hello");
 });
+function readCategories(){
+  const content = fs.readFileSync("./Data/Categories.js");
+  const categories = JSON.parse(content)
+  return categories
+}
 // app.get("/users", (req, res) => {
 //   res.json(users);
 // });
+app.post("/categories", (req,res) =>{
+  const categories = readCategories();
+  const {name} = req.body;
+  const newCategories = {id: uuid(), name: name}
+  categories.unshift(newCategories)
+  fs.writeFileSync("./Data/Categories.js", JSON.stringify(categories))
+  res.sendStatus(201)
+
+})
 app.listen(port, () => {
   console.log("App is listening at port", port);
 });
